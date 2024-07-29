efaedc5cb06b81b31b25406b628d4520ce6637e0
ArsenalHolmes <909799631@qq.com>
2024-07-29 11:10:01 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,31 +8,8 @@ import (
 
 func main() {
 	pushPlusToken := config.ConfigInstance.PushPlusToken
-	refreshTokens := config.ConfigInstance.RefreshToken
-	bilibiliCookies := config.ConfigInstance.BilibiliCookie
-	jdCookies := config.ConfigInstance.JdCookie
 	kkCookie := config.ConfigInstance.KKCookie
-	if refreshTokens != "" {
-		refreshTokenList := strings.Split(refreshTokens, ",")
-		aliCloudDisk := &platform.AliCloudDisk{}
-		for _, refreshToken := range refreshTokenList {
-			aliCloudDisk.Run(pushPlusToken, refreshToken)
-		}
-	}
-	if bilibiliCookies != "" {
-		bilibiliCookieList := strings.Split(bilibiliCookies, ",")
-		bilibili := &platform.Bilibili{}
-		for _, bilibiliCookie := range bilibiliCookieList {
-			bilibili.Run(pushPlusToken, bilibiliCookie)
-		}
-	}
-	if jdCookies != "" {
-		jdCookiesList := strings.Split(jdCookies, ",")
-		jd := &platform.JD{}
-		for _, value := range jdCookiesList {
-			jd.Run(pushPlusToken, value)
-		}
-	}
+
 	if kkCookie != "" {
 		kkCookieList := strings.Split(kkCookie, ",")
 		kk := &platform.KK{}
